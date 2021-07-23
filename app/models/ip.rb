0ef694758e1739d758504349e8fdf09a102183c5
Luis Cantillo <luissebastiancantillo@gmail.com>
2021-07-22 23:36:13 -0500

    [WIP]: Model validate url

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: ips
+#
+#  id         :bigint           not null, primary key
+#  ip         :string
+#  url_id     :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Ip < ApplicationRecord
   belongs_to :url
+  validates :ip, presence: true
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,26 @@
+# == Schema Information
+#
+# Table name: urls
+#
+#  id          :bigint           not null, primary key
+#  url         :text
+#  token       :text
+#  visit_count :integer          default(0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  domain      :string
+#
 class Url < ApplicationRecord
   include Countable
   has_many :ips
+  validates :url, :token, :domain, presence: true
+  before_validation :add_protocol_to_url
+
+  def add_protocol_to_url
+    unless self.url.blank?
+      self.url.slice!(/http:\/\/|https:\/\//)
+      self.url = "http://#{self.url}"
+    end
+  end
   
 end
