40382c536e891c938c3c0c8efb530ad943fd872d
Luis Cantillo <luissebastiancantillo@gmail.com>
2021-07-22 23:34:35 -0500

    [WIP]: Model factories

--- spec/factories/ips.rb
+++ spec/factories/ips.rb
@@ -1,6 +1,16 @@
+# == Schema Information
+#
+# Table name: ips
+#
+#  id         :bigint           not null, primary key
+#  ip         :string
+#  url_id     :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :ip do
-    ip { "MyString" }
-    references { "" }
+    ip { Faker::Internet.ip_v4_address }
+    url { FactoryBot.create(:url) }
   end
 end
--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -1,7 +1,20 @@
+# == Schema Information
+#
+# Table name: urls
+#
+#  id          :bigint           not null, primary key
+#  url         :text
+#  token       :text
+#  visit_count :integer          default(0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  domain      :string
+#
 FactoryBot.define do
   factory :url do
-    url { "MyText" }
-    token { "MyText" }
-    visit_count { "" }
+    url { Faker::Internet.url }
+    token { SecureRandom.hex(4) }
+    domain { "#{ENV['HOSTNAME']}/#{token}" }
+    visit_count { 0 }
   end
 end
