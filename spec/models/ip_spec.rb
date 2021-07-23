ef3be54dfd7d97ea043b3ef13ffb7dbe4212576b
Luis Cantillo <luissebastiancantillo@gmail.com>
2021-07-22 23:35:54 -0500

    [WIP]: Model specs

--- spec/models/ip_spec.rb
+++ spec/models/ip_spec.rb
@@ -1,5 +1,23 @@
+# == Schema Information
+#
+# Table name: ips
+#
+#  id         :bigint           not null, primary key
+#  ip         :string
+#  url_id     :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Ip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid factory' do
+    it { expect(FactoryBot.build(:ip)).to be_valid }
+  end
+  describe 'Associations' do
+    it { should belong_to(:url) }
+  end
+  describe 'Validations' do
+    it { should validate_presence_of(:ip) }
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,29 @@
+# == Schema Information
+#
+# Table name: urls
+#
+#  id          :bigint           not null, primary key
+#  url         :text
+#  token       :text
+#  visit_count :integer          default(0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  domain      :string
+#
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid factory' do
+    it { expect(FactoryBot.build(:url)).to be_valid }
+  end
+
+  describe 'Associations' do
+    it { should have_many(:ips) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:url) }
+    it { should validate_presence_of(:token) }
+    it { should validate_presence_of(:domain) }
+  end
 end
